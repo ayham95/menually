1ce4bf036f3f05587d15b78c39e3e65a89801b42
Ayham Orfali <orfali.ayham@gmail.com>
2019-06-16 21:01:52 +0300

    Added Admin panel support for offers

---
+++ app/admin/offers.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Offer do
+
+  permit_params :titleEn, :titleAr, :business_id, :descriptionAr, :descriptionEn
+
+  form do |f|
+    f.semantic_errors *f.object.errors.keys
+
+    f.inputs 'Business Details' do
+      f.input :business_id, as: :select, collection: Business.all.map {|x| [x.nameEn, x.id]}
+      f.input :titleEn
+      f.input :titleAr
+      f.input :descriptionAr
+      f.input :descriptionEn
+    end
+    f.actions
+  end
+
+end
