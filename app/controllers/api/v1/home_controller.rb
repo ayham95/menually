a9b573861295cc1dc3b79a178b34f35cb2926b28
Ayham Orfali <orfali.ayham@gmail.com>
2019-06-20 18:23:22 +0300

    fix HomeController

--- app/controllers/api/v1/home_controller.rb
+++ app/controllers/api/v1/home_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::HomeController < ApplicationController
   def index
-    categories = Category.all.map {|c| HomeCategorySerializer.new(c)}
-    render json: [categories: HomeCategorySerializer.new(Category.all), offers: Offer.all]
+    categories = Category.all.map {|c| HomeCategorySerializer.new(c).as_json}
+    render json: {categories: categories, offers: Offer.all}
   end
 end
