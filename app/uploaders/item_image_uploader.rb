6436a32a7feac809f2b4974a95477d4fdabb9478
Ayham Orfali <orfali.ayham@gmail.com>
2019-06-21 11:09:08 +0300

    changed items upload dir

--- app/uploaders/item_image_uploader.rb
+++ app/uploaders/item_image_uploader.rb
@@ -10,7 +10,7 @@ class ItemImageUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "#{model.category.business.nameEn}/menu/#{model.category.nameEn}/#{model.class.to_s.underscore}s"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
