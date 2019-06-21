305396e900e500b42d57321e39a7ea6d1a3b23a6
Ayham Orfali <orfali.ayham@gmail.com>
2019-06-21 13:03:55 +0300

    fixes

--- app/admin/categories.rb
+++ app/admin/categories.rb
@@ -9,7 +9,7 @@ ActiveAdmin.register Category do
       f.input :nameEn
       f.input :nameAr
       f.input :business_id, as: :select, collection: Business.all.map{|x| [x.nameEn, x.id]}
-      f.input :position, as: :select, collection: 1..32
+      f.input :position, as: :select, collection: params[:id].present? ? ((1..32).to_a - (Category.all.pluck :position).to_a + [(Category.find(params[:id]).position)]).sort : 1..32
     end
     f.actions
   end
