26f40cd087976c6150e4dab9f41bf08e95fe9fc5
Ayham Orfali <orfali.ayham@gmail.com>
2019-06-28 19:37:38 +0300

    fix: added protect from forgery

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -2,6 +2,7 @@ class Api::ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
   include ActionController::RequestForgeryProtection
   after_action -> {request.session_options[:skip] = true}
+  protect_from_forgery prepend: true
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
