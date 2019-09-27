e40c558d1871908edd487e4c31c8258c9e8962d1
Ayham Orfali <orfali.ayham@gmail.com>
2019-09-27 20:39:33 +0300

    Added new homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,9 @@ class PagesController < ApplicationController
   def support
     render 'pages/support'
   end
+
+
+  def index
+    render 'app/index'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  get 'pages/support'
+  root 'pages#index'
 
   namespace :api do
     namespace :v1 do
